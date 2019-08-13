95bc878e1b71ce76e3eabe6a25d0de669a230cb4
Cornayy <cornegamer@Live.nl>
2019-08-13 18:57:13 +0200

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
     Initialize Daehria.
 */
 
-const client = new Daehria({ config: path.resolve(__dirname + '/config') });
+const client = new Daehria({ config: __dirname + '/config.json' });
 client.login(client.config.token);
 client.initCommands(client.config.paths.commands);
 client.initEvents(client.config.paths.events);
