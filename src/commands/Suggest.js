172970eb650b06b5eb6f1e1a6b70f02127ae4dfe
Cornayy <cornegamer@Live.nl>
2019-08-20 22:33:38 +0200

    added suggest command

---
+++ src/commands/Suggest.js
@@ -0,0 +1,42 @@
+const { RichEmbed } = require('discord.js');
+const Command = require('../base/Command');
+
+class Suggest extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'suggest',
+            description: 'Sends your suggestion to the suggestion channel.',
+            category: 'Utility',
+            aliases: ['clear'],
+            args: ['<suggestion>']
+        });
+    }
+
+    run(message, args) {
+        if (args.length === 0) return;
+
+        const channel = this.client.channels.find(ch => ch.name === 'suggestions');
+
+        if (!channel) {
+            super.respond('There is no #suggestions channel in this server.');
+            return;
+        }
+
+        const { author } = message;
+        const suggestion = args.join(' ');
+
+        const embed = new RichEmbed()
+            .setTitle(`Suggestion by *${author.username}*`)
+            .setDescription(suggestion)
+            .setColor(0x00b405)
+            .setThumbnail(author.avatarURL)
+            .setFooter(
+                `${this.client.user.username} at ${new Date().toDateString()}`,
+                this.client.user.avatarURL
+            );
+
+        channel.send(embed);
+    }
+}
+
+module.exports = Suggest;
